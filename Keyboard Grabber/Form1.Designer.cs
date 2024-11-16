58a62bdc1387a1d752ca65fc56e106cbc0382d85
Hiranmay Roy <hiranmayroy183@gmail.com>
2024-11-16 19:59:10 +0600

    final
    final

---
+++ Keyboard Grabber/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Keyboard_Grabber
+{
+    partial class s
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxMain = new System.Windows.Forms.TextBox();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonGet = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.clipboardTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(135, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Keyboard Text Grabber";
+            // 
+            // textBoxMain
+            // 
+            this.textBoxMain.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBoxMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMain.ForeColor = System.Drawing.Color.Black;
+            this.textBoxMain.Location = new System.Drawing.Point(13, 61);
+            this.textBoxMain.Multiline = true;
+            this.textBoxMain.Name = "textBoxMain";
+            this.textBoxMain.Size = new System.Drawing.Size(582, 276);
+            this.textBoxMain.TabIndex = 1;
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.BackColor = System.Drawing.Color.DimGray;
+            this.buttonCopy.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonCopy.Location = new System.Drawing.Point(96, 360);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(75, 36);
+            this.buttonCopy.TabIndex = 2;
+            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.UseVisualStyleBackColor = false;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.Color.DimGray;
+            this.buttonSave.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonSave.Location = new System.Drawing.Point(177, 360);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 36);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonGet
+            // 
+            this.buttonGet.BackColor = System.Drawing.Color.DimGray;
+            this.buttonGet.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonGet.Location = new System.Drawing.Point(12, 360);
+            this.buttonGet.Name = "buttonGet";
+            this.buttonGet.Size = new System.Drawing.Size(75, 36);
+            this.buttonGet.TabIndex = 4;
+            this.buttonGet.Text = "Get";
+            this.buttonGet.UseVisualStyleBackColor = false;
+            this.buttonGet.Click += new System.EventHandler(this.buttonGet_Click);
+            // 
+            // s
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(607, 423);
+            this.Controls.Add(this.buttonGet);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.textBoxMain);
+            this.Controls.Add(this.label1);
+            this.Name = "s";
+            this.Text = "Keyboard Grabber";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxMain;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonGet;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer clipboardTimer;
+    }
+}
+
---
+++ Keyboard Grabber/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Keyboard_Grabber
+{
+    public partial class s : Form
+    {
+        public s()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGet_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Check if there is text in the clipboard
+                if (Clipboard.ContainsText())
+                {
+                    // Get the clipboard text
+                    string clipboardText = Clipboard.GetText();
+
+                    // Get the current time
+                    string timestamp = DateTime.Now.ToString("hh:mm:ss tt");
+
+                    // Append the clipboard text with timestamp to the text box
+                    textBoxMain.AppendText($"[{timestamp}] {clipboardText}{Environment.NewLine}");
+                }
+                else
+                {
+                    MessageBox.Show("Clipboard does not contain text.", "No Text Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Check if the textbox has any text
+                if (!string.IsNullOrWhiteSpace(textBoxMain.Text))
+                {
+                    // Copy the content of the textbox to the clipboard
+                    Clipboard.SetText(textBoxMain.Text);
+                    MessageBox.Show("Text copied to clipboard.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Text box is empty. Nothing to copy.", "No Text Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Check if the textbox has any text
+                if (!string.IsNullOrWhiteSpace(textBoxMain.Text))
+                {
+                    // Open a SaveFileDialog to allow the user to save the content
+                    using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                    {
+                        saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+                        saveFileDialog.Title = "Save Clipboard Text";
+                        saveFileDialog.FileName = "ClipboardText.txt";
+
+                        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            // Write the content of the text box to the selected file
+                            File.WriteAllText(saveFileDialog.FileName, textBoxMain.Text);
+                            MessageBox.Show("File saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Text box is empty. Nothing to save.", "No Text Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clipboardTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
